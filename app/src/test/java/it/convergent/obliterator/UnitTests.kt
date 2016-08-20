713f0d92ff26b2a21b4de48ad6f1fd423168baad
Michelangelo Altamore <michelangelo@altamore.org>
2016-08-20 14:10:19 +0200

    configured unit and integration tests

---
+++ app/src/test/java/it/convergent/obliterator/UnitTests.kt
@@ -0,0 +1,18 @@
+package it.convergent.obliterator
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+class UnitTests {
+    @Test
+    @Throws(Exception::class)
+    fun addition_isCorrect() {
+        assertEquals(4, (2 + 2).toLong())
+    }
+}
