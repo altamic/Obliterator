8317b51e21fe75d146392e35377548dc7f7e9145
Michelangelo Altamore <michelangelo@altamore.org>
2016-10-08 14:27:34 +0200

    carnet manager work

--- app/src/main/kotlin/it/convergent/obliterator/models/CarnetsManager.kt
+++ app/src/main/kotlin/it/convergent/obliterator/models/CarnetsManager.kt
@@ -6,7 +6,20 @@ import it.convergent.obliterator.hexStringToByteArray
  * Created by altamic on 08/10/16.
  */
 object CarnetsManager {
+    private val carnets: Map<String, Array<Carnet>> = emptyMap()
+
     fun isPredecessorAvailable(carnet: Carnet): Boolean {
+//        get remaining rides R from carnet
+//        calculate bucket from uid
+//        if R == total rides
+//          if !exists
+//              return false
+//        if R < total rides
+//                lookup for carnet with R - 1 rides
+//                 if exists
+//                    return true
+//                 else
+//                    return false
         return false
     }
 
@@ -15,8 +28,50 @@ object CarnetsManager {
     }
 
     fun store(carnet: Carnet) {
-//        calculate bucket from uid
-//
+        val uid = carnet.uid
+
+        val values = carnets.getOrElse(uid, defaultValue = { emptyArray<Carnet>() })
+
+        if (values.isEmpty()) {
+            val carnetArray = arrayOf(carnet)
+            carnets.entries.plusElement(mapOf(Pair(uid, carnetArray)))
+        } else { // not empty values
+            if (values.any() { it == carnet })
+                return
+
+            val oldValues = values
+            val newValues = values.asList()
+                    .reversed()
+                    .plus(carnet)
+                    .reversed() // double reverse to prepend the item
+                    .toTypedArray()
+
+            carnets.entries.minusElement(mapOf(Pair(uid, oldValues)))
+                    .plusElement(mapOf(Pair(uid, newValues)))
+        }
+
     }
 
+    private fun exists(carnet: Carnet): Boolean {
+        val uid = carnet.uid
+        val values = carnets.getOrElse(uid, defaultValue = { emptyArray<Carnet>() })
+
+        if (!values.isEmpty()) {
+            return values.any() { it == carnet }
+        }
+
+        return false
+    }
+
+    private fun find(uid: String, ridesNumber: Int): Carnet? {
+        if (carnets.keys.contains(uid)) {
+            val values = carnets.get(uid).orEmpty()
+
+            if (!values.isEmpty()) {
+                return values.first { it.remainingRides == ridesNumber }
+            }
+        }
+
+        return null
+    }
 }
