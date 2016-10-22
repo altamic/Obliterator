4ed4efa3a90068224f9ff53cd8e2e9d6155dea86
Michelangelo Altamore <michelangelo@altamore.org>
2016-10-22 14:09:17 +0200

    updates

--- app/src/main/kotlin/it/convergent/obliterator/state_machines/HceMode.kt
+++ app/src/main/kotlin/it/convergent/obliterator/state_machines/HceMode.kt
@@ -1,13 +1,76 @@
 package it.convergent.obliterator.state_machines
 
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
 /**
  * Created by altamic on 02/10/16.
  */
-class HceModeFlow {
 
+object HceMode {
+    val TAG = HceMode.javaClass.simpleName
 
-}
+    val actionName = "it.convergent.obliterator.daemoncall"
 
-class HceModeHandler {
+    private var context: Context? = null
 
+    private val atqa = byteArrayOf(0x44, 0x00)
+    private val sak  = byteArrayOf(0x00)
+
+    fun initialize(context: Context) {
+        HceMode.context = context
+    }
+
+    fun enable(uid: ByteArray, data: ByteArray) {
+        if (!initialized()) {
+            showWarning()
+            return
+        }
+
+        // prepare intent
+        val intent = Intent()
+        intent.putExtra("action", "UPLOAD")
+        intent.putExtra("atqa", atqa)
+        intent.putExtra("sak", sak)
+        intent.putExtra("uid", uid)
+        intent.putExtra("data", data)
+
+        // send configuration patch
+        setActionAnSend(intent)
+    }
+
+    fun disable() {
+        if (!initialized()) {
+            showWarning()
+            return
+        }
+
+        // reset original configuration
+        setExtraActionAndSend("DISABLE")
+    }
+
+    fun requestStatus() {
+        setExtraActionAndSend("REQSTATE")
+    }
+
+    private fun initialized(): Boolean {
+        return context == null
+    }
+
+    private fun setActionAnSend(intent: Intent) {
+        intent.action = actionName
+        context?.sendBroadcast(intent)
+    }
+
+    private fun setExtraActionAndSend(extraAction: String) {
+        val intent = Intent()
+        intent.putExtra("action", extraAction)
+        setActionAnSend(intent)
+    }
+
+    private fun showWarning() {
+        Log.w(TAG, "Context has not been initialized!")
+        Log.w(TAG, "invoke: 'initialize(context: Context)' before!")
+    }
 }
