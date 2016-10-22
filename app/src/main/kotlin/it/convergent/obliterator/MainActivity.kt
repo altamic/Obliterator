3dbec5decb0a4b871a75170874a32085d6fb4989
Michelangelo Altamore <michelangelo@altamore.org>
2016-10-22 11:35:51 +0200

    updates

--- app/src/main/kotlin/it/convergent/obliterator/MainActivity.kt
+++ app/src/main/kotlin/it/convergent/obliterator/MainActivity.kt
@@ -1,20 +1,28 @@
 package it.convergent.obliterator
 
 import android.app.Activity
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.net.ConnectivityManager
+import android.nfc.NfcAdapter
+import android.nfc.NfcManager
+import android.nfc.Tag
 import android.nfc.tech.MifareUltralight
+import android.os.Build
 import android.os.Bundle
 import android.os.Vibrator
+import android.provider.Settings
+import android.util.Log
 import android.widget.ProgressBar
 import android.widget.Toast
 import android.widget.ToggleButton
 import it.convergent.obliterator.managers.Carnets
 import it.convergent.obliterator.models.Carnet
 import it.convergent.obliterator.state_machines.AcquireCarnetFlow
+import it.convergent.obliterator.state_machines.AcquireCarnetFlow.State.*
 import it.convergent.obliterator.state_machines.AcquireCarnetHandler
-import java.lang.ref.WeakReference
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -26,6 +34,27 @@ class MainActivity: Activity(),  AcquireCarnetFlow.Callbacks {
     private val acquireCarnet by lazy { AcquireCarnetHandler(this.baseContext, this) }
 
     private val carnets by lazy { Carnets(this) }
+
+    var isPcdModeActive = false
+
+    private val EXTRA_TAG = "android.nfc.extra.TAG"
+    private val NFC_TECH_DISCOVERED = "android.nfc.action.TECH_DISCOVERED"
+
+    private val mfUltralight: String = MifareUltralight::class.java.name
+
+    private val filters = arrayOf(IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED))
+    private val techlist = arrayOf(arrayOf(mfUltralight))
+
+    val adapter: NfcAdapter by lazy {
+        (this.getSystemService(Context.NFC_SERVICE)
+                as NfcManager).defaultAdapter
+    }
+
+    // Pending intent
+    val pendingIntent: PendingIntent by lazy {
+        PendingIntent.getActivity(this, 0, Intent(this, this.javaClass)
+                .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0)
+    }
 
     // Executor
     val singleThreadExecutor: ExecutorService by lazy { Executors.newSingleThreadExecutor() }
@@ -73,6 +102,7 @@ class MainActivity: Activity(),  AcquireCarnetFlow.Callbacks {
             }
 
             override fun onError(message: String) {
+                acquireCarnet.flow.start() // start over again
                 Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
                 vibrator.vibrate(failVibration)
             }
@@ -81,8 +111,12 @@ class MainActivity: Activity(),  AcquireCarnetFlow.Callbacks {
 
     private val carnetListener by lazy { object: OnDataReceived {
                 override fun onDataReceived(maybeCarnet: Carnet?) {
-                    carnet = maybeCarnet
-                    carnetReadCallback()
+                    if (carnet == null) {
+                        acquireCarnet.flow.start() // start over again
+                    } else {
+                        carnet = maybeCarnet
+                        acquireCarnet.flow.next(CARNET_READ)
+                    }
                 }
         }
     }
@@ -109,26 +143,67 @@ class MainActivity: Activity(),  AcquireCarnetFlow.Callbacks {
     }
 
     override fun onNewIntent(intent: Intent?) {
-        if (intent != null && acquireCarnet.isNfcDiscovered(intent)) {
-            val mifareUltralight = acquireCarnet.getTag(intent)
+        if (intent != null && isNfcDiscovered(intent)) {
+            acquireCarnet.flow.next(CARNET_IN_RANGE)
+            val mifareUltralight = getTag(intent)
              if (mifareUltralight != null)
                  readTag(mifareUltralight)
         }
     }
 
     // utils
+    private fun isNfcEnabled(): Boolean {
+        return adapter.isEnabled
+    }
+
+    private fun activateNfcRequest() {
+        goToNfcSettings()
+        Toast.makeText(this, getString(R.string.activate_nfc_request),
+                Toast.LENGTH_LONG).show()
+    }
+
+    private fun goToNfcSettings() {
+        val intent: Intent
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            intent = Intent(Settings.ACTION_NFC_SETTINGS)
+        } else {
+            intent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+        }
+
+        startActivity(intent)
+    }
     private fun enableForegroundDispatchOrActivateNfcIfNeeded() {
-        if (acquireCarnet.isPcdModeActive)
-            if (acquireCarnet.isNfcEnabled())
-                acquireCarnet.enableForegroundDispatch(WeakReference(this))
-            else
-                acquireCarnet.activateNfcRequest()
+        if (isPcdModeActive)
+            if (isNfcEnabled()) {
+                adapter.enableForegroundDispatch(this, pendingIntent, filters, techlist)
+                activatePcdModeCallback()
+            } else {
+                activateNfcRequest()
+            }
     }
 
     private fun disableForegroundDispatchIfNeeeded() {
-        if (!acquireCarnet.isPcdModeActive)
-            if (acquireCarnet.isNfcEnabled())
-                acquireCarnet.disableForegroundDispatch(WeakReference(this))
+        if (!isPcdModeActive)
+            if (isNfcEnabled()) {
+                adapter.disableForegroundDispatch(this)
+                deactivatePcdModeCallback()
+            }
+    }
+
+    fun isNfcDiscovered(intent: Intent): Boolean {
+        val action = intent.action
+        return (action == NFC_TECH_DISCOVERED)
+    }
+
+    fun getTag(intent: Intent): MifareUltralight? {
+        val tag: Tag = intent.getParcelableExtra(EXTRA_TAG)
+        val techList = tag.techList
+        if (techList.any { tech -> tech.equals(MifareUltralight::class.java.name) }) {
+            carnetInRangeCallback()
+            return MifareUltralight.get(tag)
+        }
+        return null
     }
 
     private fun readTag(mifareUltralight: MifareUltralight) {
@@ -149,17 +224,34 @@ class MainActivity: Activity(),  AcquireCarnetFlow.Callbacks {
     // flows callback
     override fun startAcquireCarnet() { acquireCarnet.flow.start() }
 
+    override fun activatePcdModeCallback() {
+        Log.d(TAG, "activatePcdMode")
+        isPcdModeActive = true
+    }
+
+    override fun deactivatePcdModeCallback() {
+        Log.d(TAG, "deactivatePcdMode")
+        isPcdModeActive = false
+        acquireCarnet.flow.next(PCD_DEACTIVATED)
+    }
+
+    override fun carnetInRangeCallback() {
+        Log.d(TAG, "carnetInRange")
+    }
+
     override fun carnetReadCallback() {
-//        if (carnet != null && carnets.isPredecessorAvailable(carnet!!)) {
+        Log.d(TAG, "carnetRead: $carnet")
+//        if (carnets.isPredecessorAvailable(carnet!!)) {
 //            predecessor = carnets.predecessor(carnet!!)
-//            predecessorCarnetFoundCallback()
+//            acquireCarnet.flow.next(PREDECESSOR_FOUND)
 //
 //        } else {
-//            predecessorCarnetNotFoundCallback()
+//            acquireCarnet.flow.next(PREDECESSOR_NOT_FOUND)
 //        }
 
         predecessor = carnet
-        predecessorCarnetFoundCallback()
+        acquireCarnet.flow.next(PREDECESSOR_FOUND)
+
     }
 
     override fun predecessorCarnetNotFoundCallback() {
@@ -175,6 +267,7 @@ class MainActivity: Activity(),  AcquireCarnetFlow.Callbacks {
     }
 
     override fun acquireCarnetCompleted() {
+        Log.d(TAG, "acquireCarnetCompleted")
 //        showCarnetLayout()
 //        startHceMode(predecessor)
     }
--- app/src/main/kotlin/it/convergent/obliterator/state_machines/AcquireCarnet.kt
+++ app/src/main/kotlin/it/convergent/obliterator/state_machines/AcquireCarnet.kt
@@ -1,20 +1,7 @@
 package it.convergent.obliterator.state_machines
 
-import android.app.Activity
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.nfc.NfcAdapter
-import android.nfc.NfcManager
-import android.nfc.Tag
-import android.nfc.tech.MifareUltralight
-import android.os.Build
-import android.provider.Settings
 import android.util.Log
-import android.widget.Toast
-import it.convergent.obliterator.R
-import java.lang.ref.WeakReference
 
 /**
  * Created by altamic on 02/10/16.
@@ -25,14 +12,14 @@ class AcquireCarnetFlow(val actions: Actions, val callbacks: Callbacks) {
 
     // internal actions
     interface Actions {
-        fun activatePcdModeAction()
-        fun carnetInRangeAction()
-        fun deactivatePcdModeAction()
     }
 
     // can be triggered externally (or trigger external behaviour)
     interface Callbacks {
         fun startAcquireCarnet()
+        fun activatePcdModeCallback()
+        fun deactivatePcdModeCallback()
+        fun carnetInRangeCallback()
         fun carnetReadCallback()
         fun predecessorCarnetNotFoundCallback()
         fun predecessorCarnetFoundCallback()
@@ -61,26 +48,28 @@ class AcquireCarnetFlow(val actions: Actions, val callbacks: Callbacks) {
     )
 
     private val behaviour = mapOf(
-            Pair(State.START,             State.WAIT_FOR_CARNET)    to { actions.activatePcdModeAction() },
-            Pair(State.WAIT_FOR_CARNET,   State.CARNET_IN_RANGE)    to { actions.carnetInRangeAction() },
+            Pair(State.START,             State.WAIT_FOR_CARNET)    to { callbacks.activatePcdModeCallback() },
+            Pair(State.WAIT_FOR_CARNET,   State.CARNET_IN_RANGE)    to { callbacks.carnetInRangeCallback() },
             Pair(State.CARNET_IN_RANGE,   State.CARNET_READ)        to { callbacks.carnetReadCallback() },
             Pair(State.CARNET_READ,       State.PREDECESSOR_FOUND)  to { callbacks.predecessorCarnetFoundCallback() },
             Pair(State.PREDECESSOR_NOT_FOUND, State.WAIT_FOR_CARNET)    to { callbacks.predecessorCarnetNotFoundCallback() },
-            Pair(State.PREDECESSOR_FOUND, State.PCD_DEACTIVATED)    to { actions.deactivatePcdModeAction() },
+            Pair(State.PREDECESSOR_FOUND, State.PCD_DEACTIVATED)    to { callbacks.deactivatePcdModeCallback() },
             Pair(State.PCD_DEACTIVATED,   State.END)                to { callbacks.acquireCarnetCompleted() }
     )
 
     var currentState = State.START
 
     private fun go(current:State, next:State) {
-        if (transitions[current]!!.contains(next))
+        Log.d(TAG, "Trying to go from $currentState to $next")
+        if (transitions[current]!!.contains(next)) {
             transaction(next)
-        else
+        } else {
+            Log.d(TAG, "ERROR: $next state not found!")
             error()
+        }
     }
 
     private fun transaction(next:State) {
-        Log.d(TAG, "Trying to go from $currentState to $next")
         val oldState = currentState
         currentState = next
         val transition = behaviour[Pair(oldState, currentState)]!!
@@ -89,12 +78,13 @@ class AcquireCarnetFlow(val actions: Actions, val callbacks: Callbacks) {
         Log.d(TAG, "current state now is: $currentState")
     }
 
-    private fun next(state:State) {
+    fun next(state:State) {
         go(currentState, state)
     }
 
     fun start() {
-        next(State.START)
+        currentState = State.START
+        next(State.WAIT_FOR_CARNET)
     }
 
     fun error() {
@@ -104,86 +94,5 @@ class AcquireCarnetFlow(val actions: Actions, val callbacks: Callbacks) {
 }
 
 class AcquireCarnetHandler(val context: Context, val callbacks: AcquireCarnetFlow.Callbacks): AcquireCarnetFlow.Actions {
-
     val flow = AcquireCarnetFlow(this, this.callbacks)
-
-    var isPcdModeActive = false
-
-    private val EXTRA_TAG = "android.nfc.extra.TAG"
-    private val NFC_TECH_DISCOVERED = "android.nfc.action.TECH_DISCOVERED"
-
-    private val mfUltralight: String = MifareUltralight::class.java.name
-
-    private val filters = arrayOf(IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED))
-    private val techlist = arrayOf(arrayOf(mfUltralight))
-
-    val adapter: NfcAdapter by lazy {
-        (this.context
-                .getSystemService(Context.NFC_SERVICE)
-                as NfcManager).defaultAdapter
-    }
-
-    // Pending intent
-    val pendingIntent: PendingIntent by lazy {
-        PendingIntent.getActivity(this.context, 0, Intent(this.context, this.context.javaClass)
-                .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0)
-    }
-
-    override fun activatePcdModeAction() {
-        isPcdModeActive = true
-    }
-
-    override fun deactivatePcdModeAction() {
-        isPcdModeActive = false
-    }
-
-    override fun carnetInRangeAction() { }
-
-    fun enableForegroundDispatch(weakRefActivity: WeakReference<Activity>) {
-        adapter.enableForegroundDispatch(weakRefActivity.get(), pendingIntent, filters, techlist)
-        activatePcdModeAction()
-    }
-
-    fun disableForegroundDispatch(weakRefActivity: WeakReference<Activity>) {
-        adapter.disableForegroundDispatch(weakRefActivity.get())
-        deactivatePcdModeAction()
-    }
-
-    fun isNfcDiscovered(intent: Intent): Boolean {
-        val action = intent.action
-        return (action == NFC_TECH_DISCOVERED)
-    }
-
-    fun getTag(intent: Intent): MifareUltralight? {
-        val tag: Tag = intent.getParcelableExtra(EXTRA_TAG)
-        val techList = tag.techList
-        if (techList.any { tech -> tech.equals(MifareUltralight::class.java.name) }) {
-            carnetInRangeAction()
-            return MifareUltralight.get(tag)
-        }
-        return null
-    }
-
-    fun isNfcEnabled(): Boolean {
-        return adapter.isEnabled
-    }
-
-    fun activateNfcRequest() {
-        goToNfcSettings()
-        Toast.makeText(this.context, this.context
-                .getString(R.string.activate_nfc_request),
-                Toast.LENGTH_LONG).show()
-    }
-
-    private fun goToNfcSettings() {
-        var intent: Intent
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            intent = Intent(Settings.ACTION_NFC_SETTINGS)
-        } else {
-            intent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
-        }
-
-        this.context.startActivity(intent)
-    }
 }
