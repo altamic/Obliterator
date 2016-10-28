489cef286c0854555861b4dc03a2f36e1a0d02ac
Michelangelo Altamore <michelangelo@altamore.org>
2016-10-28 20:03:38 +0200

    updates

--- nfcd/src/main/cpp/nfcd.h
+++ nfcd/src/main/cpp/nfcd.h
@@ -8,7 +8,7 @@
 #include <stdint.h>
 #include "vendor/libnfc.h"
 
-#define LOG_TAG "NATIVENFC"
+#define LOG_TAG "Obliterator"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__ )
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__ )
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
